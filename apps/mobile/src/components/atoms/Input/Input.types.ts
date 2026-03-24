f7ed852a25ec7d1f8bed324a1b2ad69136ea9879
RafaelRivas-PT <rafaelr@prettytechnical.io>
2026-03-24 10:38:55 -0400

    docs: (src/components) add props comments

--- apps/mobile/src/components/atoms/Input/Input.types.ts
+++ apps/mobile/src/components/atoms/Input/Input.types.ts
@@ -3,14 +3,41 @@ import { type StyleProp, type TextStyle, type ViewStyle } from 'react-native';
 
 export interface InputProps
   extends Omit<PaperTextInputProps, 'style' | 'contentStyle' | 'outlineStyle'> {
+  /**
+   * Style for the input text.
+   */
   style?: StyleProp<TextStyle>;
+  /**
+   * Style for the input content wrapper.
+   */
   contentStyle?: StyleProp<TextStyle>;
+  /**
+   * Style for the input outline.
+   */
   outlineStyle?: StyleProp<ViewStyle>;
+  /**
+   * Style for the input underline.
+   */
   underlineStyle?: StyleProp<ViewStyle>;
+  /**
+   * Optional style overrides for input subparts.
+   */
   styles?: {
+    /**
+     * Container style override.
+     */
     container?: StyleProp<TextStyle>;
+    /**
+     * Content style override.
+     */
     content?: StyleProp<TextStyle>;
+    /**
+     * Outline style override.
+     */
     outline?: StyleProp<ViewStyle>;
+    /**
+     * Underline style override.
+     */
     underline?: StyleProp<ViewStyle>;
   };
 }
--- apps/mobile/src/components/atoms/Textarea/Textarea.types.ts
+++ apps/mobile/src/components/atoms/Textarea/Textarea.types.ts
@@ -3,14 +3,41 @@ import { type StyleProp, type TextStyle, type ViewStyle } from 'react-native';
 
 export interface TextareaProps
   extends Omit<PaperTextInputProps, 'style' | 'contentStyle' | 'outlineStyle' | 'multiline'> {
+  /**
+   * Style applied to textarea text.
+   */
   style?: StyleProp<TextStyle>;
+  /**
+   * Style applied to textarea content wrapper.
+   */
   contentStyle?: StyleProp<TextStyle>;
+  /**
+   * Style applied to textarea outline.
+   */
   outlineStyle?: StyleProp<ViewStyle>;
+  /**
+   * Style applied to textarea underline.
+   */
   underlineStyle?: StyleProp<ViewStyle>;
+  /**
+   * Optional style overrides for textarea subparts.
+   */
   styles?: {
+    /**
+     * Container style override.
+     */
     container?: StyleProp<TextStyle>;
+    /**
+     * Content style override.
+     */
     content?: StyleProp<TextStyle>;
+    /**
+     * Outline style override.
+     */
     outline?: StyleProp<ViewStyle>;
+    /**
+     * Underline style override.
+     */
     underline?: StyleProp<ViewStyle>;
   };
 }
